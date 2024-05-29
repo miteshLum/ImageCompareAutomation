6e2940fe4d763cb129958a4c52eecf9959908b58
miteshLum <126685002+miteshLum@users.noreply.github.com>
2024-05-30 00:00:33 +0530

    new code update

--- server/api/index.js
+++ server/api/index.js
@@ -1,23 +1,22 @@
 const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.send("Server is running");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send({ data: "abc" });
 });
 
-app.get("/allmodules", async (request, response) => {
+app.get("/allmodules", async function (req, res) {
   const suiteURL = "https://meek-salmiakki-e84eac.netlify.app/data/suites.json";
   const moduleData = await axios.get(suiteURL).then(async (response) => {
     const allModules = response.data.children;
     return allModules;
   });
   //return moduleData;
-  response.send(moduleData);
+  res.send(moduleData);
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.listen(5000, () => console.log("Server ready on port 5000."));
+
+module.exports = app;
